56260b05b6d7f77494fb54253075c6795411f6d7
SlaytonWebb <slayton.webb@connect.mjca.org>
2025-01-28 09:36:46 -0600

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,20 @@
-
+scene.setBackgroundColor(13)
+let mySprite = sprites.create(img`
+    . . . . f f f f f f . . . . . . 
+    . . . . e e e e e e . . . . . . 
+    . . . . f f e e f f . . . . . . 
+    . . . . e e e e e e . . . . . . 
+    . . . . e f f f f e . . . . . . 
+    . . . . . e e e e . . . . . . . 
+    . . c c c c c c c c c c . . . . 
+    . . c c c c c c c c c c . . . . 
+    . . c c c c c c c c c c . . . . 
+    . . c c c c c c c c c c . . . . 
+    . . e e 8 8 8 8 8 8 e e . . . . 
+    . . . . 8 8 8 8 8 8 . . . . . . 
+    . . . . 8 8 . . 8 8 . . . . . . 
+    . . . f f f . . f f f . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+tiles.setCurrentTilemap(tilemap`level4`)
